1703901e00f0967325775545b36e38a920f9750f
Piotr Kowalski <piecioshka@gmail.com>
2017-06-04 12:03:59 +0200

    Add sound when time is end

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 # countdown
 
-> Display time in countdown style
+> ⏰ Display time in countdown style
 
 ## DEMO
 
 https://piecioshka.github.io/countdown/
+
+## Features
+
+* Count down to passed time
+* You can change time during counting down by update hash and press ENTER
+* Use custom web font to display awesome numbers
+* Set red theme when time is end
+* Play alarm clock when time is end
+* Properly works on all modern browsers
+
+## How to set concrete time to countdown?
+
+You can pass into hash as formatted time ex: 
+
+* https://piecioshka.github.io/countdown/#10:00
+* https://piecioshka.github.io/countdown/#03:14
+* https://piecioshka.github.io/countdown/#100:00
+
+**Default** is one minute.
+
+![](./demo.png)
 
 ## License
 
---
+++ main.js
@@ -0,0 +1,76 @@
+const ONE_SECOND = 1000;
+const ONE_MINUTE = 60 * ONE_SECOND;
+
+let currentTime = 0;
+let limitTime = null;
+let $place = null;
+let clock = null;
+
+function playSound(src) {
+    let audio = new Audio(src);
+    audio.load();
+    audio.play();
+}
+
+function playAlarm() {
+    playSound('./audio/Alarm_clock.mp3');
+}
+
+function displayTime(seconds) {
+    $place.innerText = formatSecond(seconds);
+}
+
+function setWarningStyle() {
+    $place.classList.add('warning-text');
+    document.body.classList.add('warning-background');
+}
+
+function formatSecond(seconds) {
+    let minutesToDisplay = Math.floor(seconds / 60 / ONE_SECOND);
+    let secondsToDisplay = (seconds / ONE_SECOND) % (60);
+    return String(minutesToDisplay).padStart(2, '0')
+        + ':'
+        + String(secondsToDisplay).padStart(2, '0');
+}
+
+function getCurrentTime() {
+    return limitTime - currentTime;
+}
+
+function finishCounting() {
+    clearInterval(clock);
+    setWarningStyle();
+    playAlarm();
+}
+
+function readLimitTime() {
+    let formattedTime = location.hash.slice(1);
+    if (!formattedTime) {
+        return ONE_MINUTE;
+    }
+    let [minutes, seconds] = formattedTime.split(':');
+    minutes = Number(minutes);
+    seconds = Number(seconds);
+    if (Number.isNaN(minutes) || Number.isNaN(seconds)) {
+        return ONE_MINUTE;
+    }
+    return ONE_SECOND * (seconds + minutes * 60);
+}
+
+function setup() {
+    $place = document.querySelector('h1');
+    limitTime = readLimitTime();
+    displayTime(getCurrentTime());
+
+    clock = setInterval(() => {
+        currentTime += ONE_SECOND;
+        displayTime(getCurrentTime());
+
+        if (currentTime > limitTime - ONE_SECOND) {
+            finishCounting();
+        }
+    }, ONE_SECOND);
+}
+
+window.addEventListener('DOMContentLoaded', setup);
+window.addEventListener('hashchange', location.reload.bind(location));
